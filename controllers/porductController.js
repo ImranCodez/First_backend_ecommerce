f4d819d9d4824237cccb995c252aa6431176edd6
imrancodez <imranhossaianratul@gmail.com>
2026-03-27 05:00:37 -0700

    class22

--- controllers/porductController.js
+++ controllers/porductController.js
@@ -4,12 +4,9 @@ const sendResponse = require("../services/responsiveHandler");
 
 const createproduct = async (req, res) => {
   try {
-    // const { title, description, category, price, discountpercentage, tags } =
-    //   req.body;
-    const thumbnail = req.files?.thumbnail;
-    const images = req.files?.images;
-    // console.log("myphoto",images);
-    // return
+    const { title, description, category, price, discountpercentage, tags,variants } = req.body;
+    // const thumbnail = req.files?.thumbnail;
+    // const images = req.files?.images;
     // if (!title) return sendResponse(res, 400, "title is required");
     // if (!description) return sendResponse(res, 400, "description is required");
     // if (!category) return sendResponse(res, 400, "category is required");
@@ -18,28 +15,27 @@ const createproduct = async (req, res) => {
     //   return sendResponse(res, 400, "discountpercentage is required");
     // if (!tags) return sendResponse(res, 400, "tags is required");
     // if (!thumbnail || thumbnail?.length===0) return sendResponse(res, 400, "thumbnail is required");
-    if (images && images?.length < 4)
-      return sendResponse(res, 400, "you cant't upload images max 4");
-    const thumbnailimg = await UploadTcloudinery(thumbnail[0], "thumbnail");
-    let imagesUrl = [];
-    if (images) {
-      for (const img of images) {
-        const imgurl = await UploadTcloudinery(img, "product");
-        imagesUrl.push(imgurl.secure_url);
-      }
-    }
+    // if (images && images?.length < 4)
+    //   return sendResponse(res, 400, "you cant't upload images max 4");
+    // const thumbnailimg = await UploadTcloudinery(thumbnail[0], "thumbnail");
 
-    console.log(imagesUrl);
-
+    // let imagesUrl = [];
+    // if (images) {
+    //   for (const img of images) {
+    //     const imgurl = await UploadTcloudinery(img, "product");
+    //     imagesUrl.push(imgurl.secure_url);
+    //   }
+    // }
+    // console.log(imagesUrl);
     //     let imgurl = await Promise.all(
     //   images.map(async (image) => {
     //     const imagesUrl = await UploadTcloudinery(image, "product");
     //     return imagesUrl.secure_url;
     //   })
     // );
-
-    // console.log("arrayurl", imgurl);
-
+    
+   console.log(Array.isArray(variants))
+     if(!Array.isArray(variants)||variants.length==0) return sendResponse(res,400,"minimum 1 variansts is required");
     // const createproduct = new productSchema({
     //   title,
     //   description,
@@ -57,5 +53,7 @@ const createproduct = async (req, res) => {
     console.log(error);
   }
 };
+
+
 
 module.exports = { createproduct };
